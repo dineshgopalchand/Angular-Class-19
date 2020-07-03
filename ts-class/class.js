e3dad4b03f325c18201b8588100d6362db4e9d90
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-07-03 11:31:17 +0530

    introduced properties (set/get)  in class, fixed issue by install some module see comments mentioned in the bottom of class/class.ts

--- ts-class/class.js
+++ ts-class/class.js
@@ -6,16 +6,39 @@ function getDistance(points) {
 }
 console.log(getDistance({ maxPoint: 35, minPoint: 12 }));
 console.log(getDistance({ maxPoint: 45, minPoint: 17 }));
-var DrawPoints = /** @class */ (function () {
-    function DrawPoints(points) {
+class DrawPoints {
+    constructor(points) {
         this.points = points;
     }
-    DrawPoints.prototype.getDistance = function () {
+    getDistance() {
         return this.points.maxPoint - this.points.minPoint;
-    };
-    return DrawPoints;
-}());
-var obj1 = new DrawPoints({ maxPoint: 35, minPoint: 12 });
+    }
+    /*
+    used getter property to access private/protected properties of a class
+    */
+    get maxPoint() {
+        console.log('from getter');
+        return this.points.maxPoint;
+    }
+    set maxPoint(value) {
+        console.log('from setter');
+        this.points.maxPoint = value;
+    }
+}
+const obj1 = new DrawPoints({ maxPoint: 35, minPoint: 12 });
 console.log(obj1.getDistance());
-var obj2 = new DrawPoints({ maxPoint: 45, minPoint: 12 });
+const obj2 = new DrawPoints({ maxPoint: 45, minPoint: 12 });
 console.log(obj2.getDistance());
+console.log(obj2.maxPoint); // access getter property
+obj2.maxPoint = 65; // access setter property
+console.log(obj2.maxPoint); // access getter property
+/*
+to resolve the issue of setter and getter or use of es6/es6+ code, installed below modules
+`npm install @types/webpack`
+or this package:
+`npm install @types/html-webpack-plugin`
+
+and
+
+`npm install @types/source-map@0.5.2`
+*/
--- ts-class/class.ts
+++ ts-class/class.ts
@@ -23,9 +23,37 @@ class DrawPoints {
     getDistance() {
         return this.points.maxPoint - this.points.minPoint;
     }
+
+    /*
+    used getter property to access private/protected properties of a class
+    */
+    get maxPoint() {
+        console.log('from getter');
+        return this.points.maxPoint;
+    }
+    set maxPoint(value: number) {
+        console.log('from setter');
+        this.points.maxPoint = value;
+    }
 }
 
 const obj1 = new DrawPoints({ maxPoint: 35, minPoint: 12 });
 console.log(obj1.getDistance());
 const obj2 = new DrawPoints({ maxPoint: 45, minPoint: 12 });
 console.log(obj2.getDistance());
+console.log(obj2.maxPoint); // access getter property
+obj2.maxPoint = 65; // access setter property
+console.log(obj2.maxPoint); // access getter property
+
+
+
+/*
+to resolve the issue of setter and getter or use of es6/es6+ code, installed below modules
+`npm install @types/webpack`
+or this package:
+`npm install @types/html-webpack-plugin`
+
+and
+
+`npm install @types/source-map@0.5.2`
+*/
